59e2c66fa7d6c7119db1e0c2f2375adfdfa89b3e
OrlyIdeate <ueno.jin25@outlook.jp>
2023-09-05 20:28:00 +0900

    tweet作成処理・一覧画面実装

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -15,7 +15,7 @@ class TweetController extends Controller
     public function index()
     {
         //
-        $tweets = [];
+        $tweets = Tweet::getAllOrderByUpdated_at();
         return response()->view('tweet.index', compact('tweets'));
     }
 
@@ -33,7 +33,23 @@ class TweetController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // バリデーション
+        $validator = Validator::make($request->all(), [
+            'tweet' => 'required | max:191',
+            'description' => 'required',
+        ]);
+        // バリデーション:エラー
+        if ($validator->fails()) {
+            return redirect()
+            ->route('tweet.create')
+            ->withInput()
+            ->withErrors($validator);
+        }
+        // create()は最初から用意されている関数
+        // 戻り値は挿入されたレコードの情報
+        $result = Tweet::create($request->all());
+        // ルーティング「todo.index」にリクエスト送信（一覧ページに移動）
+        return redirect()->route('tweet.index');
     }
 
     /**
